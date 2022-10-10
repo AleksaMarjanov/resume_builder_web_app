1bac0c42ddf4772404244fcab5932a236c8fafd2
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-10-09 19:57:15 -0500

    ..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import { Education, Experience, Extras, Profile, Projects, Resume } from '../components'
+import React from "react";
+import {
+  Education,
+  Experience,
+  Extras,
+  Profile,
+  Projects,
+  Resume,
+} from "../components";
 
-interface Props {
-  
-}
+interface Props {}
 
 const Home = (props: Props) => {
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <nav>
+    <div className="flex flex-col items-center justify-center">
+      <nav className="text-4xl list-none flex items-center justify-center border-b-2 mb-5 text-white bg-blue-900 w-full">
         <ul>
-          <li className='text-4xl list-none flex items-center justify-center border-b-2 mb-5'>
-            Resume Builder
-          </li>
+          Resume Builder
         </ul>
       </nav>
-        <main>
-        <Education/>
+      <main>
+        <section>
+        <Education />
+        </section>
+        <section>
         <Experience />
+        </section>
+        <section>
         <Extras />
+        </section>
+        <section>
         <Profile />
+        </section>
+        <section>
         <Projects />
+        </section>
+        <section>
         <Resume />
-    </main>
+        </section>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
